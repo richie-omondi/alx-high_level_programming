d83e89dda697a73fc044af3823f54065617969cc
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-09-26 00:29:14 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,3 +114,16 @@ class Rectangle(Base):
             returns the area of the Rectangle instance.
         """
         return (self.__width * self.__height)
+
+    def display(self):
+        """
+            prints to stdout the Rectangle instance with '#'
+        """
+        rectangle = ""
+        print_symbol = "#"
+
+        print("\n" * self.y, end="")
+
+        for i in range(self.height):
+            rectangle += (" " * self.x) + (print_symbol*self.width) + "\n"
+        print(rectangle, end="")
