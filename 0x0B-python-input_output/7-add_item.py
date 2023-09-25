cb5ec49aff4494d8f924aa558db3cbb107074e67
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-09-25 06:00:41 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,16 +11,15 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 def add_items():
-
+    
     filename = "add_item.json"
-
     if path.isfile(filename):
-        arg_list = load_from_json_file(filename)
+        final_list = load_from_json_file(filename)
     else:
-        arg_list = []
+        final_list = []
+    for i in range(1, len(argv)):
+        final_list.append(argv[i])
+    save_to_json_file(final_list, filename)
 
-    for arg in sys.argv[1:]:
-        arg_list.append(arg)
-    save_to_json_file(arg_list, filename)
 
 add_items()
