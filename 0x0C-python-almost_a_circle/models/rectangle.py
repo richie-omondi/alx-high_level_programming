de3837cb7eecc48edf63fb958e1a102b806376db
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-09-26 00:21:28 +0300

    Create rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+"""
+    contains class Rectangle which implements Base.
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+        class Rectangle implements Base.
+        Methods:
+            __init__()
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            Initializes the instance of the class..
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+            getter function for __width
+            Returns: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            setter function for width.
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+            getter function for height
+            Returns: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            setter function for height
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+            getter function for x.
+            Returns: x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+            setter function for x.
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+            getter function for y
+            Returns: y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+            setter function for y
+            Args:
+                value (int): value to be set.
+        """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
+        self.__y = value
