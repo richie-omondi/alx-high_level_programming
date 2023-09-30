730728fd4f6bd0c8ee4442488a5784cb8a027e77
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-09-30 03:14:12 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,9 +10,6 @@ def append_write(filename="", text=""):
         filename: filename
         text: text to write
 
-    Raises
-        Exception: when the file can be opened
-
     """
 
     with open(filename, 'a', encoding="utf-8") as f:
