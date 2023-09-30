a79b7f658966d67ca4877035a6996eed928c25c3
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-09-30 03:14:51 +0300

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -11,8 +11,5 @@ def from_json_string(my_str):
     Args:
         my_str: JSON representation
 
-    Raises:
-        Exception: when the string can't be decoded
-
     """
     return json.loads(my_str)
