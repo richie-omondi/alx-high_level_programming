980c2817cfa531d7ba68105f586eb7ebfc6ef475
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-11-15 16:50:40 +0300

    Add 1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Script that prints C is fun,Python is cool and JavaScript is amazing  each on a new line.
+
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
